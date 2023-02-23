6727f1c85256af311671501ff65a9d9fa033d4d0
sultanrafeed <sultanrafeed@gmail.com>
2023-02-23 18:07:13 +0600

    1st commit

--- app/src/main/java/com/example/rokkha/alert.kt
+++ app/src/main/java/com/example/rokkha/alert.kt
@@ -52,7 +52,7 @@ class alert : AppCompatActivity() {
                 location?.let {
                     val message = "I am in danger!! Here's my location: https://www.google.com/maps/search/?api=1&query=${it.latitude},${it.longitude}"
                     val intent = Intent(Intent.ACTION_SENDTO).apply {
-                        data = Uri.parse("smsto:${+8801990188461}") // Replace phoneNumber with the phone number of your contact
+                        data = Uri.parse("smsto:${+8801732073478}") // Replace phoneNumber with the phone number of your contact
                         putExtra("sms_body", message)
                     }
                     startActivity(intent)
