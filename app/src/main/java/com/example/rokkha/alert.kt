a3b9b9b0778a3839870ad365db03b5291098b1fb
sultanrafeed <sultanrafeed@gmail.com>
2023-02-27 11:49:53 +0600

    automated sms changes

--- app/src/main/java/com/example/rokkha/alert.kt
+++ app/src/main/java/com/example/rokkha/alert.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.net.Uri
+import android.telephony.SmsManager
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -50,12 +51,16 @@ class alert : AppCompatActivity() {
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
                 location?.let {
-                    val message = "I am in danger!! Here's my location: https://www.google.com/maps/search/?api=1&query=${it.latitude},${it.longitude}"
+                    /*val message = "I am in danger!! Here's my location: https://www.google.com/maps/search/?api=1&query=${it.latitude},${it.longitude}"
                     val intent = Intent(Intent.ACTION_SENDTO).apply {
-                        data = Uri.parse("smsto:${+8801571040586}") // Replace phoneNumber with the phone number of your contact
+                        data = Uri.parse("smsto:${+8801990188461}") // Replace phoneNumber with the phone number of your contact
                         putExtra("sms_body", message)
                     }
-                    startActivity(intent)
+                    startActivity(intent)*/
+                    val message = "I am in danger!! Here's my location: https://www.google.com/maps/search/?api=1&query=${it.latitude},${it.longitude}"
+                    val sms = this.getSystemService(SmsManager::class.java)
+                    sms.sendTextMessage("01571040586", "01732073478", message, null, null)
+
                 }
             }
     }
